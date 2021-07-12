42d214b6347bef2bf9c5509e6865ea72b2df08ac
sonys03 <80260565+sonys03@users.noreply.github.com>
2021-07-12 18:04:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'немой сайт';
+$name = 'Быстрая панда из македонии';
+$image = 'img\рррр.jpg';
+$email = 'sona-zelenskaya@bk.ru';
+$phone = '+79183POSOSI';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/quotes.php');
+require('components/news.php');
+require('components/contacts.php');
+require('components/footer.php');
